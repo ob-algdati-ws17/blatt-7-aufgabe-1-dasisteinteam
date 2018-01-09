bbe4eb5ca3d3bf6f475b179bbd7973c9e0eb249f
Andreas Stiglmeier <stiglmeier.andreas@gmail.com>
2018-01-09 14:06:43 +0100

    project builds correctly

---
+++ test/testAvl.cpp
@@ -0,0 +1,9 @@
+#include "testAvl.h"
+#include <memory>
+
+using namespace std;
+
+
+TEST(AvlTest, Test_Dummy) {
+
+}
---
+++ test/testAvl.h
@@ -0,0 +1,11 @@
+#ifndef ARRAYLIST_TESTAVL_H
+#define ARRAYLIST_TESTAVL_H
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "avlBaum/AvlBaum.h"
+
+class AvlTest : public ::testing::Test {
+};
+
+#endif //ARRAYLIST_TESTLIST_H
